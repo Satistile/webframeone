5683d088a1ce4f5871d246eda4fa0532ed17e1df
Satistile <85414536+Satistile@users.noreply.github.com>
2023-02-05 18:42:04 +0100

    Add files via upload

--- resources/webframeone.js
+++ resources/webframeone.js
@@ -2,8 +2,8 @@ function wfoScroll(element) {
     document.querySelector(element).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
 
-function wfoExpandToggle(element) {
-    element.parentElement.classList.toggle('border');
-    let parent = element.parentElement;
+function wfoExpandToggle(toggleElement) {
+    toggleElement.parentElement.classList.toggle('border');
+    let parent = toggleElement.parentElement;
     parent.querySelector('.wfo.expand.content').classList.toggle('visible')
 }
