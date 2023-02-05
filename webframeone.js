8f69d5cbd685cf8b4aba09fd831ec8c8fe051370
Satistile <85414536+Satistile@users.noreply.github.com>
2023-02-05 18:41:43 +0100

    Delete webframeone.js

--- webframeone.js
+++
@@ -1,9 +0,0 @@
-function wfoScroll(element) {
-    document.querySelector(element).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-}
-
-function wfoExpandToggle(toggleElement) {
-    toggleElement.parentElement.classList.toggle('border');
-    let parent = toggleElement.parentElement;
-    parent.querySelector('.wfo.expand.content').classList.toggle('visible')
-}
