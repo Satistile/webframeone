b8561df0730f34eedaff64077182c595ab761287
Satistile <85414536+Satistile@users.noreply.github.com>
2023-04-04 21:23:54 +0200

    framework update to 1.0 ig

--- resources/webframeone.js
+++ resources/webframeone.js
@@ -2,8 +2,37 @@ function wfoScroll(element) {
     document.querySelector(element).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
 
-function wfoExpandToggle(toggleElement) {
-    toggleElement.parentElement.classList.toggle('border');
+function wfoBorderExpandToggle(toggleElement) {
+    toggleElement.parentElement.classList.toggle('active');
     let parent = toggleElement.parentElement;
-    parent.querySelector('.wfo.expand.content').classList.toggle('visible')
+    parent.querySelector('.wfo.border-expand.content').classList.toggle('active')
+}
+
+function wfoNavActiveToggle(element) {
+    parent = element.parentElement;
+    parent.classList.toggle('active');
+}
+function wfoNavDropdownToggleOpen(element) {
+    parent = element.parentElement;
+    parent.querySelector('div').classList.toggle('active');
+}
+function wfoNavDropdownToggleClose(element) {
+    element.parentElement.classList.toggle('active');
+}
+
+function wfoCheckboxChecked(checkbox) {
+    return document.querySelector(checkbox).checked;
+}
+
+function wfoColorModeToggle() {
+    document.querySelectorAll(".wfo").forEach(function(element) {
+        if((element.classList.contains('black') || element.classList.contains('white')) && !element.classList.contains('no-color-mode-toggle')) {
+            element.classList.toggle("black");
+            element.classList.toggle("white");
+        }
+        if((element.classList.contains('blacker') || element.classList.contains('whiter')) && !element.classList.contains('no-color-mode-toggle')) {
+            element.classList.toggle("blacker");
+            element.classList.toggle("whiter");
+        }
+    });
 }
