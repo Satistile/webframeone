a0a7914bb551466fcc3b53a57a8665dbb850fd8b
Satistile <85414536+Satistile@users.noreply.github.com>
2024-10-27 17:24:06 +0100

    update to v.2.1.1
    - minor design changes
    - new fade-in class

--- resources/global.js
+++ resources/global.js
@@ -6,4 +6,35 @@ class wfo {
     static CheckboxChecked(checkbox) {
         return document.querySelector(checkbox).checked;
     }
+
+    static scrollWatcher() {
+        const elements = document.querySelectorAll('.fade-in');
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                const rect = entry.boundingClientRect;
+                if (!entry.isIntersecting && (rect.bottom > window.innerHeight) && entry.target.classList.contains('visible')) {
+                    // Das Element hat den Bildschirm verlassen (oben oder unten)
+                    entry.target.classList.remove('visible');
+                } else if (entry.isIntersecting && !entry.target.classList.contains('visible')) {
+                    // Das Element ist im sichtbaren Bereich
+                    entry.target.classList.add('visible');
+                }
+            });
+        }, {
+            threshold: 0.05 // Z.B. 10% des Elements müssen sichtbar sein
+        });
+
+        elements.forEach(element => {
+            observer.observe(element);
+        });
+    }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    wfo.scrollWatcher();
+});
+
+document.addEventListener('astro:page-load', () => {
+    wfo.scrollWatcher();
+});
